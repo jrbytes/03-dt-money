b08e8648cae88d00f97e391ab6122036ea428f71
Jr Bytes <itamazor@gmail.com>
2022-08-16 18:12:13 -0300

    :tada:

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme.green500};
+  }
+
+  body {
+    background-color: ${props => props.theme.gray800};
+    color: ${props => props.theme.gray100};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem 'Fira Code', sans-serif;
+  }
+`
